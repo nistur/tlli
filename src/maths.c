67289d62782d0859466e6eb72fb46a4cf9f41371
Isak Andersson <contact@bitpuffin.com>
2015-08-19 20:59:17 +0100

    implement type dependent subtraction, also fix typo with determining type which caused it to always pick num (oops)

--- src/maths.c
+++ src/maths.c
@@ -1,8 +1,6 @@
 #include "maths.h"
 #include "tlli_internal.h"
 
-#include <stdio.h>
-
 static unsigned char tlli_Which_Arithmetic_Type(int num, tlliValue** args)
 {
 	int i;
@@ -14,7 +12,7 @@ static unsigned char tlli_Which_Arithmetic_Type(int num, tlliValue** args)
 			return TLLI_VAL_NIL;
 		}
 	}
-	return TLLI_VAL_NUM;
+	return TLLI_VAL_INT;
 }
 
 tlliValue* tlli_Num_Add(int num, tlliValue** args)
@@ -52,7 +50,8 @@ tlliValue* tlli_Int_Add(int num, tlliValue** args)
 tlliValue* tlli_Add(int num, tlliValue** args)
 {
 	unsigned char t = tlli_Which_Arithmetic_Type(num, args);
-	switch(t) {
+	switch(t)
+	{
 	case TLLI_VAL_NUM:
 		return tlli_Num_Add(num, args);
 		break;
@@ -65,7 +64,24 @@ tlliValue* tlli_Add(int num, tlliValue** args)
 	}
 }
 
-tlliValue* tlli_Sub(int num, tlliValue** args)
+tlliValue* tlli_Num_Sub(int num, tlliValue** args)
+{
+	number val = TLLI_NUMBER_ZERO;
+	int i;
+	tlliValueToNumber(args[0], &val);
+	for(i = 1; i < num; ++i)
+	{
+		number v = TLLI_NUMBER_ZERO;
+		tlliValueToNumber(args[i], &v);
+		val -= v;
+	}
+
+	tlliValue* rtn;
+	tlliNumberToValue(val, &rtn);
+	return rtn;
+}
+
+tlliValue* tlli_Int_Sub(int num, tlliValue** args)
 {
 	int val = 0;
 	int i;
@@ -80,6 +96,23 @@ tlliValue* tlli_Sub(int num, tlliValue** args)
 	tlliValue* rtn;
 	tlliIntToValue(val, &rtn);
 	return rtn;
+}
+
+tlliValue* tlli_Sub(int num, tlliValue** args)
+{
+	unsigned char t = tlli_Which_Arithmetic_Type(num, args);
+	switch(t)
+	{
+	case TLLI_VAL_NUM:
+		return tlli_Num_Add(num, args);
+		break;
+	case TLLI_VAL_INT:
+		return tlli_Int_Add(num, args);
+		break;
+	default:
+		return tlliNil;
+		break;
+	}
 }
 
 tlliValue* tlli_Mul(int num, tlliValue** args)
